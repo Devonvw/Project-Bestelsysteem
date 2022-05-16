90684811e6b81d07ab7f0ce23dfa2214839cc5f5
PieterInholland <90396110+PieterInholland@users.noreply.github.com>
2022-05-16 08:37:49 +0200

    aanpassignen dal

---
+++ Model/Reservation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }  
+        public int Persons { get; set; }    
+        public int TableId { get; set; }
+
+        public Reservation(int id, string name, DateTime dateTime, int persons, int tableId)
+        {
+            Id = id;
+            Name = name;
+            DateTime = dateTime;
+            Persons = persons;
+            TableId = tableId;
+        }
+
+        public Reservation()
+        {
+        }
+    }
+
+}
---
+++ Model/Table.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public bool Occupied { get; set; }
+
+        public Table(int id, bool occupied)
+        {
+            Id = id;
+            Occupied = occupied;
+        }
+        public Table()
+        {
+        }
+    }
+
+}
