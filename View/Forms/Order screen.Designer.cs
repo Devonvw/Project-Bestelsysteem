75f2d6d7ff989850b2142645f59e09862986c092
ShinyMorgana <jackyeichenberger@gmail.com>
2022-05-17 19:56:01 +0200

    UI Start

---
+++ View/Forms/Order screen.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace View.Forms
+{
+    partial class Order_screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.buttonTable3 = new System.Windows.Forms.Button();
+            this.buttonTable4 = new System.Windows.Forms.Button();
+            this.buttonTable6 = new System.Windows.Forms.Button();
+            this.buttonTable5 = new System.Windows.Forms.Button();
+            this.buttonTable8 = new System.Windows.Forms.Button();
+            this.buttonTable7 = new System.Windows.Forms.Button();
+            this.buttonTable10 = new System.Windows.Forms.Button();
+            this.buttonTable9 = new System.Windows.Forms.Button();
+            this.buttonTable2 = new System.Windows.Forms.Button();
+            this.buttonTable1 = new System.Windows.Forms.Button();
+            this.bottomPanel = new System.Windows.Forms.Panel();
+            this.childFormPanel = new System.Windows.Forms.Panel();
+            this.topPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // topPanel
+            // 
+            this.topPanel.Controls.Add(this.buttonTable2);
+            this.topPanel.Controls.Add(this.buttonTable1);
+            this.topPanel.Controls.Add(this.buttonTable10);
+            this.topPanel.Controls.Add(this.buttonTable9);
+            this.topPanel.Controls.Add(this.buttonTable8);
+            this.topPanel.Controls.Add(this.buttonTable7);
+            this.topPanel.Controls.Add(this.buttonTable6);
+            this.topPanel.Controls.Add(this.buttonTable5);
+            this.topPanel.Controls.Add(this.buttonTable4);
+            this.topPanel.Controls.Add(this.buttonTable3);
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(448, 100);
+            this.topPanel.TabIndex = 0;
+            // 
+            // buttonTable3
+            // 
+            this.buttonTable3.Location = new System.Drawing.Point(100, 12);
+            this.buttonTable3.Name = "buttonTable3";
+            this.buttonTable3.Size = new System.Drawing.Size(73, 30);
+            this.buttonTable3.TabIndex = 1;
+            this.buttonTable3.Text = "3";
+            this.buttonTable3.UseVisualStyleBackColor = true;
+            this.buttonTable3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonTable4
+            // 
+            this.buttonTable4.Location = new System.Drawing.Point(100, 48);
+            this.buttonTable4.Name = "buttonTable4";
+            this.buttonTable4.Size = new System.Drawing.Size(73, 30);
+            this.buttonTable4.TabIndex = 2;
+            this.buttonTable4.Text = "4";
+            this.buttonTable4.UseVisualStyleBackColor = true;
+            // 
+            // buttonTable6
+            // 
+            this.buttonTable6.Location = new System.Drawing.Point(188, 48);
+            this.buttonTable6.Name = "buttonTable6";
+            this.buttonTable6.Size = new System.Drawing.Size(73, 30);
+            this.buttonTable6.TabIndex = 4;
+            this.buttonTable6.Text = "6";
+            this.buttonTable6.UseVisualStyleBackColor = true;
+            // 
+            // buttonTable5
+            // 
+            this.buttonTable5.Location = new System.Drawing.Point(188, 12);
+            this.buttonTable5.Name = "buttonTable5";
+            this.buttonTable5.Size = new System.Drawing.Size(73, 30);
+            this.buttonTable5.TabIndex = 3;
+            this.buttonTable5.Text = "5";
+            this.buttonTable5.UseVisualStyleBackColor = true;
+            // 
+            // buttonTable8
+            // 
+            this.buttonTable8.Location = new System.Drawing.Point(278, 48);
+            this.buttonTable8.Name = "buttonTable8";
+            this.buttonTable8.Size = new System.Drawing.Size(73, 30);
+            this.buttonTable8.TabIndex = 6;
+            this.buttonTable8.Text = "8";
+            this.buttonTable8.UseVisualStyleBackColor = true;
+            // 
+            // buttonTable7
+            // 
+            this.buttonTable7.Location = new System.Drawing.Point(278, 12);
+            this.buttonTable7.Name = "buttonTable7";
+            this.buttonTable7.Size = new System.Drawing.Size(73, 30);
+            this.buttonTable7.TabIndex = 5;
+            this.buttonTable7.Text = "7";
+            this.buttonTable7.UseVisualStyleBackColor = true;
+            // 
+            // buttonTable10
+            // 
+            this.buttonTable10.Location = new System.Drawing.Point(363, 48);
+            this.buttonTable10.Name = "buttonTable10";
+            this.buttonTable10.Size = new System.Drawing.Size(73, 30);
+            this.buttonTable10.TabIndex = 8;
+            this.buttonTable10.Text = "10";
+            this.buttonTable10.UseVisualStyleBackColor = true;
+            // 
+            // buttonTable9
+            // 
+            this.buttonTable9.Location = new System.Drawing.Point(363, 12);
+            this.buttonTable9.Name = "buttonTable9";
+            this.buttonTable9.Size = new System.Drawing.Size(73, 30);
+            this.buttonTable9.TabIndex = 7;
+            this.buttonTable9.Text = "9";
+            this.buttonTable9.UseVisualStyleBackColor = true;
+            // 
+            // buttonTable2
+            // 
+            this.buttonTable2.Location = new System.Drawing.Point(12, 48);
+            this.buttonTable2.Name = "buttonTable2";
+            this.buttonTable2.Size = new System.Drawing.Size(73, 30);
+            this.buttonTable2.TabIndex = 10;
+            this.buttonTable2.Text = "2";
+            this.buttonTable2.UseVisualStyleBackColor = true;
+            // 
+            // buttonTable1
+            // 
+            this.buttonTable1.Location = new System.Drawing.Point(12, 12);
+            this.buttonTable1.Name = "buttonTable1";
+            this.buttonTable1.Size = new System.Drawing.Size(73, 30);
+            this.buttonTable1.TabIndex = 9;
+            this.buttonTable1.Text = "1";
+            this.buttonTable1.UseVisualStyleBackColor = true;
+            // 
+            // bottomPanel
+            // 
+            this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bottomPanel.Location = new System.Drawing.Point(0, 861);
+            this.bottomPanel.Name = "bottomPanel";
+            this.bottomPanel.Size = new System.Drawing.Size(448, 100);
+            this.bottomPanel.TabIndex = 1;
+            // 
+            // childFormPanel
+            // 
+            this.childFormPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.childFormPanel.Location = new System.Drawing.Point(0, 100);
+            this.childFormPanel.Name = "childFormPanel";
+            this.childFormPanel.Size = new System.Drawing.Size(448, 761);
+            this.childFormPanel.TabIndex = 2;
+            // 
+            // Order_screen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 961);
+            this.Controls.Add(this.childFormPanel);
+            this.Controls.Add(this.bottomPanel);
+            this.Controls.Add(this.topPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Order_screen";
+            this.Text = "Order screen";
+            this.topPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.Button buttonTable3;
+        private System.Windows.Forms.Button buttonTable2;
+        private System.Windows.Forms.Button buttonTable1;
+        private System.Windows.Forms.Button buttonTable10;
+        private System.Windows.Forms.Button buttonTable9;
+        private System.Windows.Forms.Button buttonTable8;
+        private System.Windows.Forms.Button buttonTable7;
+        private System.Windows.Forms.Button buttonTable6;
+        private System.Windows.Forms.Button buttonTable5;
+        private System.Windows.Forms.Button buttonTable4;
+        private System.Windows.Forms.Panel bottomPanel;
+        private System.Windows.Forms.Panel childFormPanel;
+    }
+}
